bb5e159c499e8cef0ad5e3916fdd27f9c44f429c
Magnus Haraldson Høie <39849954+Magnushhoie@users.noreply.github.com>
2020-04-15 04:23:24 +0200

    new3

--- setup.sh
+++ setup.sh
@@ -4,10 +4,10 @@
 #/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 #test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
 #test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-#test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
-#echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+#test -r ~/.bash_profile && echo -e "\neval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+#echo -e "\neval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
 
-echo "Installing brew and pip ..."
+echo -e "\nInstalling brew and pip ..."
 #install brew
 #bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
@@ -15,15 +15,13 @@ echo "Installing brew and pip ..."
 #curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 #python get-pip.py
 
-# Fuzzy finder!
+echo -e "\nInstalling fzf fuzzy finder ..."
 brew install fzf
 
-echo "Installing dependencies ..."
+echo -e "\nInstalling dependencies ..."
 #Ensure updated
 brew install vim
 brew install less
-
-# Fancy versions of bash utils
 brew install bat
 brew install ripgrep
 brew install rga
@@ -31,29 +29,36 @@ brew install diff-so-fancy
 brew install fzy
 brew install fasd
 
-echo "Installing buku, safari/chrome/firefox browser bookmark search"
+#echo -e "\nInstalling buku, safari/chrome/firefox browser bookmark search"
 # Search bookmarks from safari, chrome, firefox...
 #pip3 install --user buku
 #alias b='buku --suggest'
 #b --ai
 
-echo "Installing devdocs, offline programming documentation lookup"
+#echo -e "\nInstalling devdocs, offline programming documentation lookup"
 # DevDocs for offline documentation lookup
 #brew cask install devdocs
 
-echo "Installing tldr, tldr lookup of bash commands"
+echo -e "\nInstalling tldr, tldr lookup of bash commands"
+pip install --user tldr
 
-echo "Sourcing fuzzy_commands.sh ..."
+echo -e "\nSourcing fuzzy_commands.sh ..."
 source fuzzy_commands.sh
-echo "Done!"
+
+echo -e "Adding source to .bash_profile"
+source_file=$PWD/fuzzy_commands.sh
+LINE=$(echo 'source' $source_file)
+FILE=$(echo $HOME/test.txt)
+grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+echo -e "\nDone!"
 
 
-#echo "Installing academic references fuzzy search"
+#echo -e "\nInstalling academic references fuzzy search"
 # https://github.com/msprev/fzf-bibtex
 #brew install bib-tool
 #brew install go
 
-#echo "Installing fast pdf searcher. Use in folder with pdfs"
-#echo "Usage: p"
+#echo -e "\nInstalling fast pdf searcher. Use in folder with pdfs"
+#echo -e "\nUsage: p"
 # Fast pdf finder
 #brew install bellecp/fast-p/fast-pdf-finder
